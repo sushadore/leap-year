38fcefe0ec84bdf7cf46b24b6d54b3cb1e971592
Susha Dore and Slavik Mygovych <susha@>
2016-10-03 19:46:26 -0700

    create leap year calculator using behavior driven development method

---
+++ js/scripts.js
@@ -0,0 +1,56 @@
+var leapYear = function(year) {
+  if(year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
+
+$(document).ready(function() {
+  $("form#leap-year").submit(function(event) {
+
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+
+    $(".year").text(year);
+
+    if (result === false){
+      $(".not").show();
+    }
+
+    $("#result").show(result);
+
+    event.preventDefault();
+  });
+});
+
+// OR THIS!!!
+// business logic
+// var leapYear = function(year) {
+//   if ((year % 4 === 0) && (year % 100 !== 0) || (year % 400 === 0)) {
+//     return true;
+//   } else {
+//     return false;
+//   }
+// };
+//
+// // user interface logic
+// $(document).ready(function() {
+//   $("form#leap-year").submit(function(event) {
+//     event.preventDefault();
+//     var year = parseInt($("input#year").val());
+//     var result = leapYear(year);
+//
+//     $(".year").text(year);
+//
+//     if (!result) {                 // same as writing if (result === false)
+//       $(".not").text("not");
+//     } else {
+//       $(".not").text("");
+//     }
+//
+//     $("#result").show();
+//   });
+// });
